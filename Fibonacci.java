42c52743285298061db2852fcf69050a2b742512
nayaraporto <naya08.vitoria@gmail.com>
2025-05-09 14:07:44 -0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,7 +3,18 @@ package Aula9;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 0;
+		int b = 1;
+		int c;
+		
+		System.out.print(a + " " + b + " ");
+		
+		for(int i = 2; i < 10; i++) {
+			c = a + b;
+			System.out.print(c + " ");
+			a = b;
+			b = c;
+		}
 
 	}
 
