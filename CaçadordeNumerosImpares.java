d2d4537f5ea5511fb6bd0d53cbf898722f0886e5
nayaraporto <naya08.vitoria@gmail.com>
2025-05-09 14:08:44 -0300

    Update CaçadordeNumerosImpares.java

--- CaçadordeNumerosImpares.java
+++ CaçadordeNumerosImpares.java
@@ -3,8 +3,14 @@ package Aula9;
 public class CaçadordeNumerosImpares {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" Os números ímpares são : ");
+		for(int i = 1; i <= 50; i+= 2) {
+			
+		System.out.println(i);
+			
+		}
 
 	}
 
 }
+
