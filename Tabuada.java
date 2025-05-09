995c3586098a3846b4014c0f65dbcf7ebe868ebb
nayaraporto <naya08.vitoria@gmail.com>
2025-05-09 14:07:18 -0300

    Update Tabuada.java

--- Tabuada.java
+++ Tabuada.java
@@ -1,9 +1,18 @@
 package Aula9;
-
+import java.util.Scanner;
 public class Tabuada {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 Scanner ler = new Scanner(System.in); 
+			System.out.println("Insira um número: ");
+		int numero = ler.nextInt();
+		
+		System.out.println("Tabuada do número: " + numero + ":");
+		for(int i = 1; i <= 10; i++) {
+		System.out.println( numero + "x" + i + "=" + (numero*i));
+			
+	ler.close();
+		}
 
 	}
 
