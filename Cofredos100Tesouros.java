fc3b436c8363a42d4ff2b54b23744be2899118b9
nayaraporto <naya08.vitoria@gmail.com>
2025-05-09 14:08:23 -0300

    Update Cofredos100Tesouros.java

--- Cofredos100Tesouros.java
+++ Cofredos100Tesouros.java
@@ -3,7 +3,12 @@ package Aula9;
 public class Cofredos100Tesouros {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" Os números ímpares são : ");
+		for(int i = 1; i <= 50; i+= 2) {
+			
+		System.out.println(i);
+			
+		}
 
 	}
 
